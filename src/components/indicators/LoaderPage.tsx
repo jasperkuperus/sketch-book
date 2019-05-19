f3d79a43f181b8a74fefe1e3644888b4d973ea5e
Jasper Kuperus <bitbucket@jasperkuperus.nl>
2019-05-19 17:59:50 +0200

    Fix linting

--- src/components/indicators/LoaderPage.tsx
+++ src/components/indicators/LoaderPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import * as loaders from './loaders';
 import * as statusIndicators from './statusIndicators';
